62a9e32e3d05a46979efd8a8a183f80839c72eba
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-11-21 00:25:09 -0500

    Remove unused import

--- CoffeeScript.java
+++ CoffeeScript.java
@@ -7,7 +7,6 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import javax.script.Invocable;
-import org.mozilla.javascript.ScriptRuntime;
 
 public class CoffeeScript {
 
